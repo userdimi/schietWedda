b5762dbf2850a7084f488bdfdc199765e95dc318
Dimitri Simon <dimitri_simon@posteo.de>
2020-06-22 21:27:25 +0200

    - setup dependency injection with hilt

---
+++ app/src/main/java/de/pottcode/schietwedda/SchiedWeddaApplication.kt
@@ -0,0 +1,11 @@
+package de.pottcode.schietwedda
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * (c) Dimitri Simon on 22.06.20
+ */
+
+@HiltAndroidApp
+class SchiedWeddaApplication : Application()
